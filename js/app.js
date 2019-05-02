0ba2a6856b0f5b49f03ae376619e88a1b8d4f413
Vishesh <visheshsingh16@gmail.com>
2019-05-01 20:53:17 -0500

    regex match and output html

--- js/app.js
+++ js/app.js
@@ -1,11 +1,42 @@
 const search = document.getElementById("search");
-const matchList = document.getElementById("matchList");
+const matchList = document.getElementById("match-list");
 
 // Search states.json and filter it
 const searchStates = async searchText => {
   const res = await fetch("../data/states.json");
   const states = await res.json();
 
-  console.log(states);
+  // Get matches to current input
+  let matches = states.filter(state => {
+    const regex = new RegExp(`^${searchText}`, "gi");
+    return state.name.match(regex) || state.abbr.match(regex);
+  });
+
+  if (searchText.length === 0) {
+    matches = [];
+  }
+  console.log(matches);
+  outputHtml(matches);
+};
+
+// Show output html
+const outputHtml = states => {
+  if (states.length > 0) {
+    const html = states
+      .map(state => {
+        return `
+        <div class="card card-body mb-1">
+            <h4>${state.name} (${state.abbr}) <span class="text-info">${
+          state.capital
+        }</span></h4>
+            
+            <small>Lat: ${state.lat} and Long: ${state.long}</small>
+        </div>
+        `;
+      })
+      .join("");
+    console.log(html);
+    matchList.innerHTML = html;
+  }
 };
 search.addEventListener("input", () => searchStates(search.value));
